b6e950b0e595cc2a19170933f69e5eb87b225477
NeoQasim <139628391+NeoQasim@users.noreply.github.com>
2023-09-12 23:24:42 +0500

    completd crud using node first time

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use("/", require('./Routes/goalRoutes'))
 app.use(express.json())
-app.use(express.urlencoded((extended: false)))
-app.use("/", require("./Routes/goalRoutes"))
+app.use(express.urlencoded(({extended: false})))
+app.use("/", require("./Routes/"))
 
 app.listen(3001, () => console.log("server runing on 3001"))
