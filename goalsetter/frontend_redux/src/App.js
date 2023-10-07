d227d93f5149a9433189bf6e124d0831859f99db
neoqasim <neoqasim@gmail.com>
2023-10-07 23:15:19 +0500

    started frontend

---
+++ goalsetter/frontend_redux/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Headers from './comppnents/Headers';
+
+function App() {
+  return (
+    <>
+      <h1><Headers /></h1>
+    </>
+  )
+}
+
+export default App;
---
+++ goalsetter/frontend_redux/src/comppnents/Headers.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Headers() {
+    return (
+        <Navbar expand="lg" className="bg-info text-light">
+            <Container className='text-warning'>
+                <Navbar.Brand href="#home">GS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    ); 
+    
+}
+
+export default Headers;
